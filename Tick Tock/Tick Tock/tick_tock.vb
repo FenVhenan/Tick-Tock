fed3e3572f225e08e7ef2ae55b03045aa9ebe12f
Jessica Craw <jess_l_craw@yahoo.com>
2017-05-21 12:30:01 -0400

    Add files via upload

--- Tick Tock/Tick Tock/tick_tock.vb
+++ Tick Tock/Tick Tock/tick_tock.vb
@@ -8,11 +8,15 @@
 
     Private Sub tmrDeath_Tick(sender As Object, e As EventArgs) Handles tmrDeath.Tick
         txtCountDown.Text = (CDbl(txtCountDown.Text) - 0.1).ToString("N1")
+        If txtCountDown.txt = (0).ToString("N1") Then
+            tmrDeath.Enabled = False
+            MessageBox.Show("Death, The bomb has gone off!")
+        End If
     End Sub
 
     Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
         If txtPassword.Text = code Then
-            MessageBox.Show("Password Accepted!")
+            MessageBox.Show("Hooray, Password Accepted!")
             tmrDeath.Enabled = False
         Else
             txtPassword.Clear()
